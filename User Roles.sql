29e01e7e46e295ee277e43f58d1fbb840a45f397
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-07 12:11:25 +0530

    Create User Roles.sql

---
+++ User Roles.sql
@@ -0,0 +1,20 @@
+USE [sstdb]
+GO
+
+/****** Object:  Table [dbo].[UserRoles]    Script Date: 05-07-2017 17:25:15 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[UserRoles](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Role] [nvarchar](max) NULL,
+ CONSTRAINT [PK_dbo.UserRoles] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
